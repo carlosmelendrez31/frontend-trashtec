955dbed206256d166d2e98297e68108e4a5eb2ca
3lReymond <ramonbatidas@gmail.com>
2025-04-07 20:08:46 -0700

    ayuda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,16 @@ import { LoginComponent } from './login/login.component';  // Componente de logi
 import { RegistroComponent } from './registro/registro.component';
 import { ActualizarDatosComponent } from './actualizar-datos/actualizar-datos.component';
 import { CarritoComponent } from './carrito/carrito.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
 
 export const routes: Route[] = [
- // { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirige a login si no está autenticado
-  //{ path: 'login', component: LoginComponent }, // Ruta de login
-  { path: '', component: HomeComponent }, // Ruta por defecto al Login
-  { path: 'home', component: HomeComponent }, // Ruta de home
-  {path:'registro',component:RegistroComponent},
-  {path:'login',component:LoginComponent},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'actualizar-datos', component: ActualizarDatosComponent },
-  {path : 'carrito', component : CarritoComponent}
-
-  // otras rutas...
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'ayuda', component: AyudaComponent }  // <-- Esta es la nueva ruta
 ];
 
   
